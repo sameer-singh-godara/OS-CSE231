3db956925bc3788feaf5d491c93236b439622eb3
Sameer Singh Godara <sameer22439@iiitd.ac.in>
2023-09-13 21:57:40 +0530

    adding graph

--- OS Assignment 2/Question 2/1.c
+++ OS Assignment 2/Question 2/1.c
@@ -18,16 +18,25 @@ int main ()
     pid_t pid1;
     pid_t pid2;
     pid_t pid3;
-
+    double executionTimes[3];
+    int policies[3];
     double execution_time1, execution_time2, execution_time3;
+    
+    FILE* file = fopen("output.txt", "w");
+    if (file == NULL) {
+        perror("Error opening file");
+        return 1;
+    }
 
     struct timespec start_time1, start_time2, start_time3, end_time1, end_time2, end_time3; // defining the datatype of start and end time of each processes
     pid1 = fork();// fork for process 1
     if (pid1==0){
         pid_t rc = fork(); // calling internal fork for caclulating time for completing the process easily
+        clock_gettime(CLOCK_REALTIME, &start_time1);
         if (!rc){
             struct sched_param param;
             param.sched_priority = 0;
+            policies[SCHED_OTHER] = SCHED_OTHER;
             
             int check_sched_set = sched_setscheduler(0, SCHED_OTHER, &param);
             if (check_sched_set == -1){
@@ -40,11 +49,13 @@ int main ()
             perror("execl error in proces 1\n");
             exit(1);
         }
-        clock_gettime(CLOCK_REALTIME, &start_time1);
+        // clock_gettime(CLOCK_REALTIME, &start_time1);
         wait(NULL);
         clock_gettime(CLOCK_REALTIME, &end_time1);
         
         execution_time1 = (end_time1.tv_sec - start_time1.tv_sec) + (double)(end_time1.tv_nsec - start_time1.tv_nsec)/1e9;
+        executionTimes[SCHED_OTHER] = execution_time1;
+        fprintf(file, "%d %lf\n", SCHED_OTHER, executionTimes[SCHED_OTHER]); // Printing output in output.txt
         printf("OTHER time : %lf\n", execution_time1);
 
     }
@@ -53,13 +64,15 @@ int main ()
         exit(1);
     }
     else{
-        wait(NULL);
+        // wait(NULL);
         pid2 = fork(); // fork for process 2
         if (!pid2 && pid1){
             pid_t rc = fork(); // calling internal fork for caclulating time for completing the process easily
+            clock_gettime(CLOCK_REALTIME, &start_time2);
             if (!rc){
                 struct sched_param param;
                 param.sched_priority = 1;
+                policies[SCHED_RR] = SCHED_RR;
 
                 int check_sched_set = sched_setscheduler(0, SCHED_RR, &param);
                 if (check_sched_set == -1){
@@ -72,11 +85,12 @@ int main ()
                 perror("execl error in proces 2\n");
                 exit(1);
             }
-            clock_gettime(CLOCK_REALTIME, &start_time2);
+            // clock_gettime(CLOCK_REALTIME, &start_time2);
             wait(NULL);
             clock_gettime(CLOCK_REALTIME, &end_time2);
-
             execution_time2 = (end_time2.tv_sec - start_time2.tv_sec) + (double)(end_time2.tv_nsec - start_time2.tv_nsec)/1e9;
+            executionTimes[SCHED_RR] = execution_time2;
+            fprintf(file, "%d %lf\n", SCHED_RR, executionTimes[SCHED_RR]); // Printing output in output.txt
             printf("RR time : %lf\n", execution_time2);
 
         }
@@ -85,14 +99,16 @@ int main ()
             exit(1);
         }
         else{
-            wait(NULL);
+            // wait(NULL);
             pid3 = fork(); // fork for process 3
             if (!pid3 && pid1 && pid2){
                 pid_t rc = fork(); // calling internal fork for caclulating time for completing the process easily
+                clock_gettime(CLOCK_REALTIME, &start_time3);
                 if (!rc){
                     struct sched_param param;
                     param.sched_priority = 1;
-
+    
+                    policies[SCHED_FIFO] = SCHED_FIFO;
                     int check_sched_set = sched_setscheduler(0, SCHED_FIFO, &param);
                     if (check_sched_set == -1){
                         perror("sched_scheduler error in process 3\n");
@@ -104,10 +120,13 @@ int main ()
                     perror("execl error in proces 3\n");
                     exit(1);
                 }
-                clock_gettime(CLOCK_REALTIME, &start_time3);
+                
+                // clock_gettime(CLOCK_REALTIME, &start_time3);
                 wait(NULL);
                 clock_gettime(CLOCK_REALTIME, &end_time3);
                 execution_time3 = (end_time3.tv_sec - start_time3.tv_sec) + (double)(end_time3.tv_nsec - start_time3.tv_nsec)/1e9;
+                executionTimes[SCHED_FIFO] = execution_time3;
+                fprintf(file, "%d %lf\n", SCHED_FIFO, executionTimes[SCHED_FIFO]); // Printing output in output.txt
                 printf("FIFO time : %lf\n", execution_time3);
             }
             else if (pid3<0){
@@ -115,16 +134,19 @@ int main ()
                 exit(1);
             }
             else{
-                wait(NULL);
+                // wait(NULL);
             }
         }
     }
 
-    // wait(NULL);
-    // wait(NULL);
-    // wait(NULL);
+    wait(NULL);
+    wait(NULL);
+    wait(NULL);
+    
+    
 
-
+    
+    fclose(file);
 
 
     
