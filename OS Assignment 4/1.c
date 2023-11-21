048e5b888b043123fde33a6f7eb4f7f90548f0d6
sameer-singh-godara <sameer22439@iiitd.ac.in>
2023-11-21 19:27:27 +0530

    1st done

--- OS Assignment 4/1.c
+++ OS Assignment 4/1.c
@@ -1,86 +1,108 @@
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
 #include <unistd.h>
 
 #define NUM_PHILOSOPHERS 5
-#define NUM_BOWLS 2
+#define NUM_FORKS 5
+#define BOWLS 2
+int eats = 2;
 
-pthread_mutex_t forks[NUM_PHILOSOPHERS];
-pthread_mutex_t bowls[NUM_BOWLS];
-pthread_cond_t cond[NUM_PHILOSOPHERS];
+pthread_mutex_t forks[NUM_PHILOSOPHERS] = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t bowls_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t masterlock = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t bowls_mutex1 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t bowls_mutex2 = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t bowls_cond = PTHREAD_COND_INITIALIZER;
 
-void eating(int philosopher_id) {
-    printf("Philosopher %d is eating\n", philosopher_id);
-    usleep(rand() % 1000000); // Simulate eating time
-}
-
-void thinking(int philosopher_id) {
-    printf("Philosopher %d is thinking\n", philosopher_id);
-    usleep(rand() % 1000000); // Simulate thinking time
-}
-
-void* philosopher(void* args) {
+void philosopher(void* args) {
     int philosopher_id = *(int*)args;
-
-    while (1) {
-        // Thinking
+    
+    while (1){
         thinking(philosopher_id);
+        pthread_mutex_lock(&masterlock);
+        int left_fork = philosopher_id;
+        int right_fork = (philosopher_id + 1) % NUM_PHILOSOPHERS;
 
-        // Pick up forks
-        pthread_mutex_lock(&forks[philosopher_id]);
-        pthread_mutex_lock(&forks[(philosopher_id + 1) % NUM_PHILOSOPHERS]);
+        pthread_mutex_lock(&forks[left_fork]);
+        pthread_mutex_lock(&forks[right_fork]);
+        while (pthread_mutex_trylock(&bowls_mutex1) != 0 && pthread_mutex_trylock(&bowls_mutex2) != 0){
+            pthread_cond_wait(&bowls_cond, &masterlock);
+        }
+        pthread_mutex_unlock(&masterlock);
 
-        // Pick up bowls
-        pthread_mutex_lock(&bowls[0]);
-        pthread_mutex_lock(&bowls[1]);
-
-        // Enter eating state
         eating(philosopher_id);
 
-        // Release forks
-        pthread_mutex_unlock(&forks[philosopher_id]);
-        pthread_mutex_unlock(&forks[(philosopher_id + 1) % NUM_PHILOSOPHERS]);
+        pthread_mutex_unlock(&bowls_mutex1);
+        pthread_mutex_unlock(&bowls_mutex2);
+    
+        pthread_mutex_unlock(&forks[left_fork]);
+        pthread_mutex_unlock(&forks[right_fork]);
+        
+        pthread_cond_signal(&bowls_cond);
 
-        // Release bowls
-        pthread_mutex_unlock(&bowls[0]);
-        pthread_mutex_unlock(&bowls[1]);
+
+        // thinking(philosopher_id);
     }
+}
+
+void eating(int philosopher_id) {
+    // pthread_mutex_lock(&bowls_mutex);
+    // int flag = 0;
+    // while (1) {
+    //     if (pthread_mutex_trylock(&bowls_mutex1) == 0) {
+    //         flag = 1;
+    //         break;
+    //     }
+    //     else if (pthread_mutex_trylock(&bowls_mutex2) == 0) {
+    //         flag = 2;
+    //         break;
+    //     }
+    //     pthread_cond_wait(&bowls_cond, &bowls_mutex);
+    // }
+
+    printf("Philosopher %d is eating\n", philosopher_id);
+    sleep(1); // Simulate eating time
+
+    // // Release the bowls
+    // if (flag == 1) {
+    //     pthread_mutex_unlock(&bowls_mutex1);
+    // } else {
+    //     pthread_mutex_unlock(&bowls_mutex2);
+    // }
+    // pthread_cond_signal(&bowls_cond);
+    // pthread_mutex_unlock(&bowls_mutex);
+}
+
+void thinking(int id) {
+    printf("philosopher %d is thinking\n", id);
+    sleep(1); // Simulate thinking time
 }
 
 int main() {
-    pthread_t philosophers[NUM_PHILOSOPHERS];
-    int philosopher_ids[NUM_PHILOSOPHERS];
+    pthread_t philosopher_threads[NUM_PHILOSOPHERS];
 
-    // Initialize mutexes and condition variables
+    // Initialize mutexes and condition variable
+    pthread_mutex_init(&bowls_mutex, NULL);
+    pthread_mutex_init(&masterlock, NULL);
+    pthread_mutex_init(&bowls_mutex1, NULL);
+    pthread_mutex_init(&bowls_mutex2, NULL);
+    pthread_cond_init(&bowls_cond, NULL);
+
     for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
         pthread_mutex_init(&forks[i], NULL);
-        pthread_mutex_init(&cond[i], NULL);
-    }
-
-    for (int i = 0; i < NUM_BOWLS; i++) {
-        pthread_mutex_init(&bowls[i], NULL);
     }
 
     // Create philosopher threads
     for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
-        philosopher_ids[i] = i;
-        pthread_create(&philosophers[i], NULL, philosopher, &philosopher_ids[i]);
+        int* philosopher_id = (int*)malloc(sizeof(int));
+        *philosopher_id = i;
+        pthread_create(&philosopher_threads[i], NULL, philosopher, philosopher_id);
     }
 
-    // Wait for threads to finish (this will never happen in this example)
+    // Wait for philosopher threads to finish
     for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
-        pthread_join(philosophers[i], NULL);
-    }
-
-    // Destroy mutexes and condition variables
-    for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
-        pthread_mutex_destroy(&forks[i]);
-        pthread_mutex_destroy(&cond[i]);
-    }
-
-    for (int i = 0; i < NUM_BOWLS; i++) {
-        pthread_mutex_destroy(&bowls[i]);
+        pthread_join(philosopher_threads[i], NULL);
     }
 
     return 0;
--- OS Assignment 4/temp.c
+++ OS Assignment 4/temp.c
@@ -10,57 +10,73 @@ int eats = 2;
 
 pthread_mutex_t forks[NUM_PHILOSOPHERS] = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t bowls_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t masterlock = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t bowls_mutex1 = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t bowls_mutex2 = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t bowls_cond = PTHREAD_COND_INITIALIZER;
 
 void philosopher(void* args) {
     int philosopher_id = *(int*)args;
-    thinking(philosopher_id);
+    
     while (1){
+        thinking(philosopher_id);
+        pthread_mutex_lock(&masterlock);
         int left_fork = philosopher_id;
         int right_fork = (philosopher_id + 1) % NUM_PHILOSOPHERS;
-        
+
         pthread_mutex_lock(&forks[left_fork]);
         pthread_mutex_lock(&forks[right_fork]);
+        while (pthread_mutex_trylock(&bowls_mutex1) != 0 && pthread_mutex_trylock(&bowls_mutex2) != 0){
+            pthread_cond_wait(&bowls_cond, &masterlock);
+        }
+        pthread_mutex_unlock(&masterlock);
+
         eating(philosopher_id);
+
+        pthread_mutex_unlock(&bowls_mutex1);
+        pthread_mutex_unlock(&bowls_mutex2);
+    
         pthread_mutex_unlock(&forks[left_fork]);
         pthread_mutex_unlock(&forks[right_fork]);
-        thinking(philosopher_id);
+        
+        pthread_cond_signal(&bowls_cond);
+
+
+        // thinking(philosopher_id);
     }
 }
 
 void eating(int philosopher_id) {
-    pthread_mutex_lock(&bowls_mutex);
-    int flag = 0;
-    while (1) {
-        if (pthread_mutex_trylock(&bowls_mutex1) == 0) {
-            flag = 1;
-            break;
-        }
-        else if (pthread_mutex_trylock(&bowls_mutex2) == 0) {
-            flag = 2;
-            break;
-        }
-        pthread_cond_wait(&bowls_cond, &bowls_mutex);
-    }
+    // pthread_mutex_lock(&bowls_mutex);
+    // int flag = 0;
+    // while (1) {
+    //     if (pthread_mutex_trylock(&bowls_mutex1) == 0) {
+    //         flag = 1;
+    //         break;
+    //     }
+    //     else if (pthread_mutex_trylock(&bowls_mutex2) == 0) {
+    //         flag = 2;
+    //         break;
+    //     }
+    //     pthread_cond_wait(&bowls_cond, &bowls_mutex);
+    // }
 
     printf("Philosopher %d is eating\n", philosopher_id);
     sleep(1); // Simulate eating time
 
-    // Release the bowls
-    if (flag == 1) {
-        pthread_mutex_unlock(&bowls_mutex1);
-    } else {
-        pthread_mutex_unlock(&bowls_mutex2);
-    }
-    pthread_cond_signal(&bowls_cond);
-    pthread_mutex_unlock(&bowls_mutex);
+    // // Release the bowls
+    // if (flag == 1) {
+    //     pthread_mutex_unlock(&bowls_mutex1);
+    // } else {
+    //     pthread_mutex_unlock(&bowls_mutex2);
+    // }
+    // pthread_cond_signal(&bowls_cond);
+    // pthread_mutex_unlock(&bowls_mutex);
 }
 
 void thinking(int id) {
-    sleep(1); // Simulate thinking time
     printf("philosopher %d is thinking\n", id);
+    sleep(1); // Simulate thinking time
 }
 
 int main() {
@@ -68,6 +84,7 @@ int main() {
 
     // Initialize mutexes and condition variable
     pthread_mutex_init(&bowls_mutex, NULL);
+    pthread_mutex_init(&masterlock, NULL);
     pthread_mutex_init(&bowls_mutex1, NULL);
     pthread_mutex_init(&bowls_mutex2, NULL);
     pthread_cond_init(&bowls_cond, NULL);
